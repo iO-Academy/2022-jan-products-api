4a5e24fa52dad11f6f73d3b42614e0417629a8ab
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-04-25 16:52:53 +0100

    working on adding product

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -11,6 +11,17 @@ let getAllProducts = async(req, res) => {
     res.json(apiResponse)
 }
 
+let addSingleProduct = async(req, res) => {
+    let connection = await DbService()
+    console.log(req.body)
+    let sku = req.body.SKU
+    let name = req.body.name
+    let price = req.body.price
+    let stock_level = req.body.stock_level
+    let products = await ProductService.addSingleProduct(connection, sku, name, price, stock_level)
+
+}
 
 module.exports.getAllProducts = getAllProducts
+module.exports.addSingleProduct = addSingleProduct
 
