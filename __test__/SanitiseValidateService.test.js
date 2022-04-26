94ab579f0ec6546e64209684ef8393792390d9f4
Sami Shanagher <sshan90@hotmail.com>
2022-04-26 16:10:52 +0100

    added sanitisePrice testing

--- __test__/SanitiseValidateService.test.js
+++ __test__/SanitiseValidateService.test.js
@@ -6,4 +6,12 @@ describe('sanitisePrice', () => {
         expect(DataCheckers.sanitisePrice('50.56')).toBe(50.56)
     })
 
+    test('sanitisePrice Failure', () => {
+        expect(DataCheckers.sanitisePrice('string')).toBe(false)
+    })
+
+    test('sanitisePrice Malformed', () => {
+        expect(DataCheckers.sanitisePrice([])).toBe(false)
+    })
+
 })
